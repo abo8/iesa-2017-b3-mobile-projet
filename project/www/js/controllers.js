e76a1e17ec18e87da89fd26f825337703a132d31
amaillot <amaillot01@gmail.com>
2017-04-27 12:29:55 +0200

    Upload Plugin Camera

--- project/www/js/controllers.js
+++ project/www/js/controllers.js
@@ -1,6 +1,41 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+
+
+  var callback = function(buttonIndex) {
+    var sourceType = navigator.camera.PictureSourceType.PHOTOLIBRARY;
+    if(buttonIndex == 2) {
+      sourceType = navigator.camera.PictureSourceType.CAMERA;
+    }
+
+    var cameraOptions = {
+      quality: 50,
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: sourceType
+    };
+
+    var cameraSuccess = function(imageURI) {
+      $scope.$apply(function () {
+        $scope.image = imageURI;
+      });
+    };
+
+    navigator.camera.getPicture(cameraSuccess, null, cameraOptions);
+  };
+
+  $scope.sheet  = function() {
+    var options = {
+      // 'androidTheme': window.plugins.actionsheet.ANDROID_THEMES.THEME_HOLO_LIGHT,
+      'buttonLabels': ['Library', 'Camera'],
+      'androidEnableCancelButton' : true, // default false
+      'winphoneEnableCancelButton' : true, // default false
+      'addCancelButtonWithLabel': 'Cancel',
+      'position': [20, 40] // for iPad pass in the [x, y] position of the popover
+    };
+    window.plugins.actionsheet.show(options, callback);
+  };
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
